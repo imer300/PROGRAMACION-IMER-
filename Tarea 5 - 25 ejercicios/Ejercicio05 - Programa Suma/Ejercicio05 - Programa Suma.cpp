0412060506db75a434293b1cd27696b2dd0c1215
imer300 <98670396+imer300@users.noreply.github.com>
2022-02-23 20:26:54 -0600

    Add files via upload

---
+++ Tarea 5 - 25 ejercicios/Ejercicio05 - Programa Suma/Ejercicio05 - Programa Suma.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+	
+	int vari1=0, vari2=0, total=0;
+	
+	cout<<"Digite un valor a sumar"<<endl;
+	cin>>vari1;
+	
+	cout<<"Digite siguiente  valor a sumar"<<endl;
+	cin>>vari2;
+	
+	total=vari1+vari2;
+	cout<<"el valor total de la suma es:"<<total;
+	
+	return 0;
+}
