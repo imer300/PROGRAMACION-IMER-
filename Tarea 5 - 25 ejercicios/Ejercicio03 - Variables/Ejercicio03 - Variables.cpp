a516a5e69760c021e2a43850ac2cfae34ecc0dc0
imer300 <98670396+imer300@users.noreply.github.com>
2022-02-23 20:25:23 -0600

    Add files via upload

---
+++ Tarea 5 - 25 ejercicios/Ejercicio03 - Variables/Ejercicio03 - Variables.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	double decimales=0;
+	char a=0;
+	
+	cout<<"escriba un numero "<<endl;
+	cin>>decimales;
+	
+	cout<<"escriba una letra o simbolo"<<endl;
+	cin>>a;
+	
+	return 0;
+}
