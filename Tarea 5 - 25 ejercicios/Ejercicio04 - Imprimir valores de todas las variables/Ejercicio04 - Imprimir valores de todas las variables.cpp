45cfd3f5e9be8c4cee6fe34f1cc641af334804b4
imer300 <98670396+imer300@users.noreply.github.com>
2022-02-23 20:26:11 -0600

    Add files via upload

---
+++ Tarea 5 - 25 ejercicios/Ejercicio04 - Imprimir valores de todas las variables/Ejercicio04 - Imprimir valores de todas las variables.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main (){
+	
+	int en=20;
+	double decimal=15.22;
+	char vocal='i';
+	float deci=13.5;
+	
+	cout<<"VALOR DE ENTERO:"<<en <<endl;
+	
+	cout<<"VALOR DE DOUBLE  :"<<decimal <<endl; 
+	
+	cout<<"VALOR DE FLOAT :"<<deci <<endl; 
+
+	cout<<"VALOR DE CHAR :"<<vocal <<endl;   
+	
+	
+	
+	return 0;
+}
