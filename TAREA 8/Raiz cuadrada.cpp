4be5d2059aca70fd8f3b3ffbd35502756595a579
imer300 <98670396+imer300@users.noreply.github.com>
2022-03-23 16:27:53 -0600

    Add files via upload

---
+++ TAREA 8/Raiz cuadrada.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+void Raizcuadrada()  //variable 1
+{
+	double x=0,resultado=0;
+	cout<<"Calculador de Raiz caudrada"<<endl<<endl;
+	cout<<"ingrese el valor al cual desea calcular la raiz cuadrada"<<endl;
+	cin>>x;	
+	
+	resultado=sqrt(x);
+	cout<<endl;
+	cout<<"La raiz cuadrada es :"<<resultado<<endl;
+	
+	
+	
+	
+}
---
+++ TAREA 8/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Raizcuadrada.h"
+
+
+int main(int argc, char** argv) {
+	
+	Raizcuadrada();
+	return 0;
+}
